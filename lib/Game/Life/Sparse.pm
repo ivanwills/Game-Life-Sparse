4c593ea62bf6d7f753b8d9c5fefc574d23fa5f07
Ivan Wills <ivan.wills@gmail.com>
2015-01-05 07:21:49 +1100

    Moopsified

--- lib/Game/Life/Sparse.pm
+++ lib/Game/Life/Sparse.pm
@@ -1,157 +1,144 @@
-package Game::Life::Sparse;
+use Moops -strict;
 
-# Created on: 2014-05-09 18:20:54
-# Create by:  Ivan Wills
-# $Id$
-# $Revision$, $HeadURL$, $Date$
-# $Revision$, $Source$, $Date$
+class Game::Life::Sparse 0.0.1 {
+    use List::Util qw/reduce/;
 
-use Moose;
-use namespace::autoclean;
-use version;
-use Carp;
-use List::Util qw/reduce/;
-use Data::Dumper qw/Dumper/;
-use English qw/ -no_match_vars /;
+    has dimentions => (
+        is       => 'ro',
+        isa      => Int,
+        required => 1,
+    );
+    has initial_length => (
+        is       => 'ro',
+        isa      => Int,
+        default  => 10,
+        required => 1,
+    );
+    has cells => (
+        is       => 'ro',
+        isa      => Int,
+        default  => 10,
+        required => 1,
+    );
+    has board => (
+        is         => 'rw',
+        isa        => HashRef[Bool],
+        builder    => '_board',
+        required   => 1,
+        lazy_build => 1,
+    );
 
-our $VERSION = version->new('0.0.1');
-
-has dimentions => (
-    is       => 'ro',
-    isa      => 'Int',
-    required => 1,
-);
-has initial_length => (
-    is       => 'ro',
-    isa      => 'Int',
-    default  => 10,
-    required => 1,
-);
-has cells => (
-    is       => 'ro',
-    isa      => 'Int',
-    default  => 10,
-    required => 1,
-);
-has board => (
-    is         => 'rw',
-    isa        => 'HashRef[Bool]',
-    builder    => '_board',
-    required   => 1,
-    lazy_build => 1,
-);
-
-sub init {
-    my ($self, $board) = @_;
-    my $new_board = {};
-    for my $i ( 0 .. @{$board} - 1 ) {
-        my $row = $board->[$i];
-        my @cells = split //, $row;
-        for my $j ( 0 .. @cells - 1 ) {
-            next if $cells[$j] eq ' ';
-            $new_board->{"$i,$j"} = 1;
+    sub init {
+        my ($self, $board) = @_;
+        my $new_board = {};
+        for my $i ( 0 .. @{$board} - 1 ) {
+            my $row = $board->[$i];
+            my @cells = split //, $row;
+            for my $j ( 0 .. @cells - 1 ) {
+                next if $cells[$j] eq ' ';
+                $new_board->{"$i,$j"} = 1;
+            }
         }
+
+        $self->board($new_board);
+
+        return $self;
     }
 
-    $self->board($new_board);
-
-    return $self;
-}
-
-sub volume {
-    my ($self) = @_;
-    my @max;
-    for my $cell (keys %{ $self->board }) {
-        my @points = split /,/, $cell;
-        for my $i (0 .. $#points) {
-            $max[$i] = $points[$i] if !$max[$i] || $points[$i] > $max[$i];
+    sub volume {
+        my ($self) = @_;
+        my @max;
+        for my $cell (keys %{ $self->board }) {
+            my @points = split /,/, $cell;
+            for my $i (0 .. $#points) {
+                $max[$i] = $points[$i] if !$max[$i] || $points[$i] > $max[$i];
+            }
         }
+
+        return reduce {$a * $b} @max;
     }
 
-    return reduce {$a * $b} @max;
-}
+    sub is_alive {
+        my ($self, $cell) = @_;
 
-sub is_alive {
-    my ($self, $cell) = @_;
-
-    return !!$self->board->{ join ',', @$cell };
-}
-
-sub next_state {
-    my ($self, $cell) = @_;
-
-    my $neighbours = $self->neighbours($cell);
-    my $count = reduce {$a + $b} map {$_ ? 1 : 0} @$neighbours;
-
-    if ( $self->is_alive($cell) ) {
-        return $count == 2 || $count == 3;
+        return !!$self->board->{ join ',', @$cell };
     }
 
-    return $count == 2;
-}
+    sub next_state {
+        my ($self, $cell) = @_;
 
-sub neighbours {
-    my ($self, $cell) = @_;
-    my $offsets = [];
-    for (1 .. $self->dimentions) {
-        $offsets = _expander($offsets);
-    }
-    my @neighbours;
-    for my $offset (@{$offsets}) {
-        # skip the 0 offset position
-        next if !reduce {$a + $b} map {abs $_} @$offset;
-        push @neighbours, $self->board->{ join ',', @{ _array_sum($cell, $offset) } };
-    }
+        my $neighbours = $self->neighbours($cell);
+        my $count = reduce {$a + $b} map {$_ ? 1 : 0} @$neighbours;
 
-    return \@neighbours;
-}
-
-sub size {
-    my ($self) = @_;
-    return scalar keys %{$self->board};
-}
-
-sub _board {
-    my ($self) = @_;
-    my $board = {};
-
-    while (keys %{$board} < $self->cells) {
-        my @pos;
-        while (@pos < $self->dimentions) {
-            push @pos, int rand $self->initial_length;
+        if ( $self->is_alive($cell) ) {
+            return $count == 2 || $count == 3;
         }
-        $board->{ join ',', @pos } = 1;
+
+        return $count == 2;
     }
 
-    return $board;
-}
-
-sub _expander {
-    my ($array) = @_;
-    $array ||= [];
-    my $dims = @$array ? @$array : 1;
-    my @new;
-
-    for my $offset (-1,0,1) {
-        for my $dim (0 .. $dims -1) {
-            push @new, [ @{ $array->[$dim] || [] }, $offset ];
+    sub neighbours {
+        my ($self, $cell) = @_;
+        my $offsets = [];
+        for (1 .. $self->dimentions) {
+            $offsets = _expander($offsets);
         }
+        my @neighbours;
+        for my $offset (@{$offsets}) {
+            # skip the 0 offset position
+            next if !reduce {$a + $b} map {abs $_} @$offset;
+            push @neighbours, $self->board->{ join ',', @{ _array_sum($cell, $offset) } };
+        }
+
+        return \@neighbours;
     }
 
-    return \@new;
-}
-
-sub _array_sum {
-    my ($array1, $array2) = @_;
-    my @new;
-    for my $i ( 0 .. $#$array1 ) {
-        $new[$i] = $array1->[$i] + $array2->[$i];
+    sub size {
+        my ($self) = @_;
+        return scalar keys %{$self->board};
     }
 
-    return \@new;
-}
+    sub _board {
+        my ($self) = @_;
+        my $board = {};
 
-__PACKAGE__->meta->make_immutable;
+        while (keys %{$board} < $self->cells) {
+            my @pos;
+            while (@pos < $self->dimentions) {
+                push @pos, int rand $self->initial_length;
+            }
+            $board->{ join ',', @pos } = 1;
+        }
+
+        return $board;
+    }
+
+    sub _expander {
+        my ($array) = @_;
+        $array ||= [];
+        my $dims = @$array ? @$array : 1;
+        my @new;
+
+        for my $offset (-1,0,1) {
+            for my $dim (0 .. $dims -1) {
+                push @new, [ @{ $array->[$dim] || [] }, $offset ];
+            }
+        }
+
+        return \@new;
+    }
+
+    sub _array_sum {
+        my ($array1, $array2) = @_;
+        my @new;
+        for my $i ( 0 .. $#$array1 ) {
+            $new[$i] = $array1->[$i] + $array2->[$i];
+        }
+
+        return \@new;
+    }
+
+};
 
 1;
 
