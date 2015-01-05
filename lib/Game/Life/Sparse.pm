946f13c3bcb56150a21fc70d9f6fa4aeb324adcd
Ivan Wills <ivan.wills@gmail.com>
2015-01-05 18:32:14 +1100

    Updated POD

--- lib/Game/Life/Sparse.pm
+++ lib/Game/Life/Sparse.pm
@@ -146,12 +146,11 @@ __END__
 
 =head1 NAME
 
-Game::Life::Sparse - <One-line description of module's purpose>
+Game::Life::Sparse - Conway's game of life; sparse version
 
 =head1 VERSION
 
 This documentation refers to Game::Life::Sparse version 0.0.1
-
 
 =head1 SYNOPSIS
 
@@ -164,66 +163,51 @@ This documentation refers to Game::Life::Sparse version 0.0.1
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
-
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
+=over 4
 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
+=item C<init ($self, $board)>
 
-Name the section accordingly.
+Initialise the board of size C<$board>
 
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
+=item C<volume ($self)>
 
+Find the boards volume
 
+=item C<is_alive ($self, $cell)>
 
+Determine if the cell C<$cell> is a alive or dead
+
+=item C<next_state ($self, $cell)>
+
+Determine the next state of a cell
+
+=item C<neighbours ($self, $cell)>
+
+Find all the neighbouts of the cell C<$cell>
+
+=item C<size ($self)>
+
+The dimention of the board
+
+=item C<_board ($self)>
+
+=item C<_expander ($array)>
+
+=item C<_array_sum ($array1, $array2)>
+
+=back
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
@@ -234,7 +218,6 @@ Patches are welcome.
 =head1 AUTHOR
 
 Ivan Wills - (ivan.wills@gmail.com)
-<Author name(s)>  (<contact address>)
 
 =head1 LICENSE AND COPYRIGHT
 
